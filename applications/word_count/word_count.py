5c9e6bad3d4ae38db098dcf0f1b8ecec2bb908fe
jackskim <jackskim29@gmail.com>
2020-07-14 20:39:26 -0700

    Add solution for word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,27 @@
 def word_count(s):
-    # Your code here
+    counts = {}
+    to_ignore = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'
 
+    # convert to lower case
+    s = s.lower()
+
+    # split words into list
+    string_list = s.split()
+
+    # iterate through the words
+    for word in string_list:
+        # strip out invalid chars
+        word = word.strip(to_ignore)
+
+        # If there are no valid chars, skip to the next word
+        if word == '': continue
+
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
 
 
 if __name__ == "__main__":
@@ -8,3 +29,5 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('" : ; , . - + = / \ | [ ] { } ( ) * ^ &'))
+
